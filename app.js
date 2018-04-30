d6a63b26c6cda030c1d262010561200421e928fa
Shirshendu Bhowmick <shirshendu_0@yahoo.co.in>
2018-04-30 22:50:06 +0530

    User sign up process added

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 const express = require('express');
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose')
+const userModel = require('./models/models');
+const mongouri = require('./config/mongodb');
 
 const app = express();
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
     res.send("Hello");
 });
 
+app.post('/signup', (req, res) => {
+    mongoose.connect(mongouri).then(() => {
+        user = new userModel({email: req.body.email,
+                              password: req.body.password,
+                              firstName: req.body.firstName,
+                              lastName: req.body.lastName,
+                              phone: req.body.phone,
+                              isActive: true});
+        user.save().then((msg) => {
+            res.send(msg);
+        }).catch((errorObj) => {
+            res.status(400).send({error: errorObj.message});
+            // res.status(400).send(errorObj);
+        });
+    }).catch((errorObj) => {
+        res.status(500).send({error: errorObj.name});
+    });
+});
 
 app.listen(3000, () => {
     console.log("Server running on port localhost port 3000");
--- models/models.js
+++ models/models.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
-const mongouri = require('../config/mongodb');
+// const mongouri = require('../config/mongodb');
 
 const userSchema = new mongoose.Schema({
-    email: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
-    phone: {type: String, required: false},
+    phone: {type: String, required: false, unique: true},
     password: {type: String, required: true},
     isActive: {type: Boolean, required: true}
 });
 
 const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
 // mongoose.connect(mongouri);
 
 // user = new userModel({email: "shirshendu_0@yahoo.co.in", password: "abc123", firstName: "Shirshendu", lastName: "Bhowmick", isActive: true});
