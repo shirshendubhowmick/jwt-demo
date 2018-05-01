91f3cdbb7b7c6daabdb9aa03645e35ba2b4150ba
Shirshendu Bhowmick <shirshendu_0@yahoo.co.in>
2018-05-01 18:13:54 +0530

    JWT generation & authentication middleware added

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken');
 const userModel = require('./models/models');
 const mongouri = require('./config/mongodb');
 
 const app = express();
+const secret_key = "thats_really_a_secret";
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    res.send("Hello");
+const authenticate = (req, res, next) => {
+    let token = req.header('x-auth');
+    try {
+        decoded = jwt.verify(token, secret_key);
+        req.decoded = decoded;
+    }
+    catch (error) {
+        res.status(401).send({error: "Invalid token, access denied"});
+    }
+    next();
+};
+
+app.get('/users', authenticate, (req, res) => {
+    res.send(req.decoded);
 });
 
 app.post('/signup', (req, res) => {
@@ -22,7 +36,9 @@ app.post('/signup', (req, res) => {
                               phone: req.body.phone,
                               isActive: true});
         user.save().then((msg) => {
-            res.send(msg);
+            let token = jwt.sign({email: req.body.email, _id: msg._id}, secret_key)
+            res.header('x-auth', token).send({email: req.body.email});
+            mongoose.connection.close();
         }).catch((errorObj) => {
             res.status(400).send({error: errorObj.message});
             // res.status(400).send(errorObj);
