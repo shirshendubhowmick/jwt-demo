b7ceba5ed132e737be358add8682eeffeed8b30d
Shirshendu Bhowmick <shirshendu_0@yahoo.co.in>
2018-05-06 03:10:20 +0530

    comments added

--- app.js
+++ app.js
@@ -12,18 +12,18 @@ const secret_key = "thats_really_a_secret";
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 
-const authenticate = (req, res, next) => {
+const authenticate = (req, res, next) => {  //The authenticate middleware
     let token = req.header('x-auth');
     try {
-        decoded = jwt.verify(token, secret_key);
+        decoded = jwt.verify(token, secret_key);  //verify token
         mongoose.connect(mongouri).then(() => {
-            userModel.findOne({_id: decoded._id, tokens: token}).then((user) => {
+            userModel.findOne({_id: decoded._id, tokens: token}).then((user) => {  //if token verification is successful then search database with _id and token
                 if(user) {
                     req.user = user;
-                    next();
+                    next();  //executes when successfully authenticated
                 }
                 else {
-                    res.status(401).send({error: "User doesn't exists, access denied."});
+                    res.status(401).send({error: "User doesn't exists, access denied."});   //throw error if authentication is unsuccessful
                 }
             }).catch((errorObj) => {
                 res.status(401).send({error: errorObj.name});
@@ -33,49 +33,49 @@ const authenticate = (req, res, next) => {
         });
     }
     catch (error) {
-        res.status(401).send({error: "Invalid token, access denied."});
+        res.status(401).send({error: "Invalid token, access denied."});  // throw error if token verification fails
     }
 };
 
-app.get('/users', authenticate, (req, res) => {
+app.get('/users', authenticate, (req, res) => {  // the GET users route, uses authenticate middleware to perform user authentication
     res.send({id: req.user._id, firstName: req.user.firstName, lastName: req.user.lastName, email: req.user.email});
 });
 
-app.post('/signup', (req, res) => {
+app.post('/signup', (req, res) => { // the POST signup route
     mongoose.connect(mongouri).then(() => {
-        user = new userModel({email: req.body.email,
+        user = new userModel({email: req.body.email,     // create new model with supplied user data
                               password: req.body.password,
                               firstName: req.body.firstName,
                               lastName: req.body.lastName,
                               phone: req.body.phone,
                               isActive: true});
-        user.save().then((msg) => {
+        user.save().then((msg) => {   // save data to database, if mongoose validation passes
             res.send({status: "ok", msg: "User created."});
             mongoose.connection.close();
         }).catch((errorObj) => {
-            res.status(400).send({error: errorObj.message});
+            res.status(400).send({error: errorObj.message});    // if mongoose validation fails throw error
         });
     }).catch((errorObj) => {
         res.status(500).send({error: errorObj.name});
     });
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', (req, res) => {  // the POST login route, use to login users i.e. generate access tokens
     mongoose.connect(mongouri).then(() => {
-        userModel.findOne({email: req.body.email}).then((user) => {
+        userModel.findOne({email: req.body.email}).then((user) => {  // retrive user data from database
             if(user && req.body.password) {
-                if(! bcrypt.compareSync(req.body.password, user.password)) {
+                if(! bcrypt.compareSync(req.body.password, user.password)) {   // check if there is a password match
                     mongoose.connection.close();
-                    res.status(401).send({error: "Authentication unsuccessful, access denied."});
+                    res.status(401).send({error: "Authentication unsuccessful, access denied."}); // if password matching fails throw error
                 }
-                let token = jwt.sign({email: user.email, _id: user._id}, secret_key);
-                user.tokens.push(token);
+                let token = jwt.sign({email: user.email, _id: user._id}, secret_key); //else sign a token
+                user.tokens.push(token); // add token to database
                 user.save().then((msg) => {
                     mongoose.connection.close();
-                    res.header('x-auth', token).send({id: msg._id, firstName: msg.firstName, lastName: msg.lastName, email: msg.email});
+                    res.header('x-auth', token).send({id: msg._id, firstName: msg.firstName, lastName: msg.lastName, email: msg.email});  // send token and user data as response
                 }).catch((errorObj) => {
                     mongoose.connection.close();
-                    res.status(400).send({error: errorObj.message});
+                    res.status(400).send({error: errorObj.message});  //if user data is invalid throw error
                 });
             }
             else {
@@ -90,14 +90,14 @@ app.post('/login', (req, res) => {
     });
 });
 
-app.delete('/logout', authenticate, (req, res) => {
+app.delete('/logout', authenticate, (req, res) => { // the DELETE logout route i.e. destroy tokens, logout route should work for only authenticated user, so authenticate middleware is used
     mongoose.connect(mongouri).then(() => {
-        userModel.update({_id: req.user._id}, { $pull: {tokens: req.header('x-auth')}}).then((data) =>{
+        userModel.update({_id: req.user._id}, { $pull: {tokens: req.header('x-auth')}}).then((data) =>{  // delete token from database
             if (data.nModified === 1 && data.ok === 1) {
-                res.send({status: "ok", msg: "User logged out."});
+                res.send({status: "ok", msg: "User logged out."});  // success response if token deletion is successful
             }
             else {
-                res.status(400).send({error: "Error logging out user."});    
+                res.status(400).send({error: "Error logging out user."});    // if token deletion is not successful then throw error
             }
         }).catch((errorObj) => {
             res.status(400).send({error: errorObj.message});
--- models/models.js
+++ models/models.js
@@ -12,9 +12,9 @@ const userSchema = new mongoose.Schema({
     isActive: {type: Boolean, required: true}
 });
 
-userSchema.pre('save', function(next) {
+userSchema.pre('save', function(next) {    //Mongoose middleware, performs password hashing before saving data into database
     let user = this;
-    if(user.isModified('password')) {
+    if(user.isModified('password')) {  //Check if the password is modifed, if so then only perform hasing
         bcrypt.genSalt(10, (error, salt) => {
             bcrypt.hash(user.password, salt, (error, hash) => {
                 user.password = hash;
